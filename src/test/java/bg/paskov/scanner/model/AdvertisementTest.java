6d935fe87df14fc7c49511cb7eecce49fea214f4
Pasko Paskov <paskopaskov0@gmail.com>
2026-04-26 13:06:16 +0300

    test: cleanup AdvertisementTest and fix package name typo

--- src/test/java/bg/paskov/scanner/model/AdvertisementTest.java
+++ src/test/java/bg/paskov/scanner/model/AdvertisementTest.java
@@ -1,37 +1,49 @@
-package bg.pаskov.scanner.model;
+package bg.paskov.scanner.model;
 
-import bg.paskov.scanner.model.Advertisement;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class AdvertisementTest {
+    private Advertisement ad1;
+
+    @BeforeEach
+    void setUp() {
+         ad1 = new Advertisement("Title A", "Link A", "ID123");
+
+    }
 
     @Test
-    @DisplayName("Should be equal if IDs match, regardless of title or link")
+    void  ad1ShouldNotBeNull() {
+        assertNotNull(ad1);
+    }
+
+    @Test
     void testEquality() {
-        Advertisement ad1 = new Advertisement("Title A", "Link A", "ID123");
         Advertisement ad2 = new Advertisement("Title B", "Link B", "ID123");
 
-       Assertions.assertEquals(ad1, ad2, "Ads with same ID must be equal");
+        assertEquals(ad1, ad2, "Ads with same ID must be equal");
     }
 
     @Test
-    @DisplayName("Should have same hash code if IDs match")
     void testHashCode() {
-        Advertisement ad1 = new Advertisement("Title A", "Link A", "ID123");
         Advertisement ad2 = new Advertisement("Title B", "Link B", "ID123");
 
-        Assertions.assertEquals(ad1.hashCode(), ad2.hashCode());
+        assertEquals(ad1.hashCode(), ad2.hashCode());
     }
 
     @Test
-    @DisplayName("Should NOT be equal if IDs are different")
     void testNotEqual() {
-        Advertisement ad1 = new Advertisement("Title A", "Link A", "ID123");
         Advertisement ad2 = new Advertisement("Title B", "Link B", "ID153");
 
-        Assertions.assertNotEquals(ad1, ad2, "Ads with different IDs should not be equal");
+        assertNotEquals(ad1, ad2, "Ads with different IDs should not be equal");
+    }
+
+    @Test
+    void testReflexive() {
+        Advertisement ad = new Advertisement("Title", "Link", "ID123");
+        assertEquals(ad, ad);
     }
 
 }
